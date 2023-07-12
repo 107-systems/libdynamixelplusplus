322a74c560dacee61374d6cf88bf4887c3f5aa3c
Alexander Entinger <consulting@lxrobotics.com>
2023-07-12 12:59:18 +0200

    [examples] allow to override default parameter via CMD line parameter.

--- examples/example-01-ping/example-01-ping.cpp
+++ examples/example-01-ping/example-01-ping.cpp
@@ -11,6 +11,7 @@
 
 #include <cstdlib>
 
+#include <sstream>
 #include <iostream>
 
 #include <dynamixel++/dynamixel++.h>
@@ -27,9 +28,26 @@ using namespace dynamixelplusplus;
 
 int main(int argc, char **argv) try
 {
-  Dynamixel dynamixel_ctrl("/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4NNZ55-if00-port0",
+  size_t      const DEFAULT_BAUD_RATE   = 115200;
+  std::string const DEFAULT_DEVICE_NAME = "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4NNZ55-if00-port0";
+
+  size_t baud_rate{DEFAULT_BAUD_RATE};
+  std::string device_name{DEFAULT_DEVICE_NAME};
+
+  if (argc > 1) {
+    device_name = std::string(argv[1]);
+  }
+  if (argc > 2) {
+    std::stringstream baud_rate_ss;
+    baud_rate_ss << argv[2];
+    baud_rate_ss >> baud_rate;
+  }
+
+  std::cout << "configured for \"" << device_name << "\" with a baud rate of \"" << baud_rate << "\"." << std::endl;
+
+  Dynamixel dynamixel_ctrl(device_name,
                            Dynamixel::Protocol::V2_0,
-                           115200);
+                           baud_rate);
 
   auto const id_vect = dynamixel_ctrl.broadcastPing();
 
