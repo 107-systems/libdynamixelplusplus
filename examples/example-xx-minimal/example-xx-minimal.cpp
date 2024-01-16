3958a7abbb005ab14d790f4d40098e3df7e21fac
Alexander Entinger <cto@lxrobotics.com>
2024-01-16 06:09:15 +0100

    Fix: use the correct register for writing the goal position: MX28_ControlTable_GoalPosition .

--- examples/example-xx-minimal/example-xx-minimal.cpp
+++ examples/example-xx-minimal/example-xx-minimal.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv) try
   /* Set goal position. */
   std::map<Dynamixel::Id, uint32_t> goal_position_data_map;
   for (auto id : id_vect) goal_position_data_map[id] = (rand() % 4096);
-  dynamixel_ctrl.syncWrite(MX28_ControlTable_Torque_Enable, goal_position_data_map);
+  dynamixel_ctrl.syncWrite(MX28_ControlTable_GoalPosition, goal_position_data_map);
 
   /* Read current position. */
   std::map<Dynamixel::Id, uint32_t> const position_map = dynamixel_ctrl.syncRead<uint32_t>(MX28_ControlTable_PresentPosition, id_vect);
